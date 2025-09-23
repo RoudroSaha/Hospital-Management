c48dd431394fa5e19827019875910fadfd5d5196
RoudroSaha <roudro1538@gmail.com>
2025-09-23 17:00:36 +0600

    add all templates

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'blood_group']
+    search_fields = ['user__first_name', 'user__last_name']
