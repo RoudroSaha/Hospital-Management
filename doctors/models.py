add101cb4245022e3c804960345cd2e057f38bd1
RoudroSaha <roudro1538@gmail.com>
2025-10-11 17:32:32 +0600

    update doctors apps

--- doctors/models.py
+++ doctors/models.py
@@ -1,16 +1,52 @@
 from django.db import models
-from django.contrib.auth.models import User
-from core.models import Department
+from users.models import Users, Doctors
+from datetime import datetime
 
-class Doctor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    specialization = models.CharField(max_length=100)
-    license_number = models.CharField(max_length=50)
-    experience = models.PositiveIntegerField(default=0)
-    consultation_fee = models.DecimalField(max_digits=10, decimal_places=2)
-    bio = models.TextField(blank=True)
-    is_available = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return f"Dr. {self.user.get_full_name()}"
+class Category(models.Model):
+  id_category = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=50)
+
+  class Meta:
+    verbose_name = "Category"
+    verbose_name_plural = "Categories"
+
+  def __str__(self):
+    return self.name
+
+
+class Blogs(models.Model):
+  blog_id = models.AutoField(primary_key=True)
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  summary = models.TextField()
+  is_published = models.BooleanField(default=False)
+  posted_at = models.DateField(default=datetime.now)
+  thumbnail = models.ImageField(upload_to="blogs/thumbnail", null=True, blank=True)
+  id_category = models.ForeignKey(Category, on_delete=models.PROTECT)
+  doctor = models.ForeignKey(Doctors, on_delete=models.CASCADE)
+
+
+  class Meta:
+    verbose_name = "Blog"
+    verbose_name_plural = "Blogs"
+
+  def __str__(self):
+    return self.title
+
+
+
+class Comments(models.Model):
+  comment_id = models.AutoField(primary_key=True)
+  content = models.TextField()
+  commented_at = models.DateField(default=datetime.now)
+  user = models.ForeignKey(Users, on_delete=models.CASCADE)
+  blog = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+
+  class Meta:
+    verbose_name = "Comment"
+    verbose_name_plural = "Comments"
+
+  def __str__(self):
+    return f"Comment by {self.user.username} on {self.blog.title}"
+
+
